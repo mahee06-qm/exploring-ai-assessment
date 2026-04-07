4756274eb3b182988f31de4cfedebe56730164b6
ulaahmahee-arch <ulaahmahee@gmail.com>
2026-04-07 11:58:29 +0100

    Linear Graph line

--- assessment_exploring_ai.py
+++ assessment_exploring_ai.py
@@ -1,22 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Load datasets
 df_preds = pd.read_csv('final_automation_predictions.csv')
-df_occ = pd.read_csv('Occupation_Data.csv')
+df_ab = pd.read_csv('AB.csv')
+
+# Filter for 'Manual Dexterity' Level
+df_dex = df_ab[(df_ab['Element Name'] == 'Manual Dexterity') & (df_ab['Scale Name'] == 'Level')]
 
 # Merge
 df_plot = pd.merge(df_preds[['O*NET-SOC Code', 'Resilience_Score']], 
-                   df_occ[['O*NET-SOC Code', 'Title']], 
+                   df_dex[['O*NET-SOC Code', 'Data Value']], 
                    on='O*NET-SOC Code')
 
-# Get Top 20
-top_20 = df_plot.sort_values(by='Resilience_Score', ascending=False).head(20)
-
 # Plot
-plt.figure(figsize=(12, 8))
-plt.barh(top_20['Title'], top_20['Resilience_Score'], color='teal')
-plt.gca().invert_yaxis()
-plt.title('Top 20 Most Resilient Occupations')
-plt.xlabel('Resilience Score')
+plt.figure(figsize=(10, 6))
+sns.regplot(data=df_plot, x='Data Value', y='Resilience_Score', 
+            scatter_kws={'alpha':0.4}, line_kws={'color':'red'})
+plt.title('Manual Dexterity Level vs. Resilience Score')
+plt.xlabel('Manual Dexterity (Level)')
+plt.ylabel('Resilience Score')
 plt.show()
