25abefba2f7cdac90a023d1a58c3fe697368c1e5
ulaahmahee-arch <ulaahmahee@gmail.com>
2026-04-07 11:57:27 +0100

    Top 20 Resilience Jobs Bar Chart

--- assessment_exploring_ai.py
+++ assessment_exploring_ai.py
@@ -1,23 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Load datasets
 df_preds = pd.read_csv('final_automation_predictions.csv')
-df_wa = pd.read_csv('WA.csv')
-
-# Filter for 'Assisting and Caring for Others' Level
-df_care = df_wa[(df_wa['Element Name'] == 'Assisting and Caring for Others') & (df_wa['Scale Name'] == 'Level')]
+df_occ = pd.read_csv('Occupation_Data.csv')
 
 # Merge
-df_plot = pd.merge(df_preds[['O*NET-SOC Code', 'Predicted_Risk_Level']], 
-                   df_care[['O*NET-SOC Code', 'Data Value']], 
+df_plot = pd.merge(df_preds[['O*NET-SOC Code', 'Resilience_Score']], 
+                   df_occ[['O*NET-SOC Code', 'Title']], 
                    on='O*NET-SOC Code')
 
+# Get Top 20
+top_20 = df_plot.sort_values(by='Resilience_Score', ascending=False).head(20)
+
 # Plot
-plt.figure(figsize=(8, 6))
-sns.boxplot(data=df_plot, x='Predicted_Risk_Level', y='Data Value', palette='Set2')
-plt.title('Assisting and Caring for Others by Risk Level')
-plt.xlabel('Predicted Risk Level')
-plt.ylabel('Assisting and Caring for Others (Level)')
+plt.figure(figsize=(12, 8))
+plt.barh(top_20['Title'], top_20['Resilience_Score'], color='teal')
+plt.gca().invert_yaxis()
+plt.title('Top 20 Most Resilient Occupations')
+plt.xlabel('Resilience Score')
 plt.show()
